80ed5ed96435a5f669a42455b1f0093ea7f896e8
Ryuhei Yokokawa <ryuhei@y-designs.com>
2020-03-12 14:18:42 -0700

    worked on adding in sentiment

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors')
 const fetch = require('node-fetch')
+const Sentiment = require('sentiment');
 
 const corsOptions = {
   origin: [
       'http://localhost:9000',
-      'http://portfolio.noindex.co:9000',
-      'http://portfolio.noindex.co:8000', 
+      'http://localhost:8000',
       'http://portfolio.noindex.co',
       'https://portfolio.noindex.co'
     ],
@@ -27,9 +27,26 @@ router.get('/history/:month/:date', cors(corsOptions), function(req, res, next) 
   fetch(apiCall)
   .then(res => res.json())
   .then(body => {
-    res.json(body)
+    let result = body.data.Events ? injectSentiment(body) : []
+    res.json(result)
   })
 })
+
+const injectSentiment = (body)=> {
+  let events = body.data.Events.reverse() //year,text
+  let event_sentiment = []
+  let sentiment = new Sentiment();
+  events.forEach(event => {
+    let wSentiment = {
+      year: event.year,
+      text: event.text,
+      sentiment: sentiment.analyze(event.text)
+    }
+    event_sentiment.push(wSentiment)
+  })
+  return event_sentiment
+}
+
 
 //Maybe APOD? https://api.nasa.gov/planetary/apod?api_key=2mZ0n02qsUz2wGaHq2XWofoEFfXlNGRmEBpFJ0Rf
 
